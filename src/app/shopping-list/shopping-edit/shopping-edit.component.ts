ce7ae7a24a328562f5585629014152fbf81e2d32
warlord-x <gaurang.developer@gmail.com>
2017-12-30 20:28:30 +0100

    dynamic ingredients add

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+
+  @Output() ingredientAddEvent =  new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addIngredient() {
+    this.ingredientAddEvent.emit(new Ingredient(this.nameInput.nativeElement.value,this.amountInput.nativeElement.value));
   }
 
 }
