9ec9e7f889661b39942ff77445c7b309a1ba0f11
warlord-x <gaurang.developer@gmail.com>
2018-01-03 12:01:09 +0100

    ingredients to shopping list using service

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {ShoppingListService} from '../../shopping-list/shopping-list.service';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,9 +11,12 @@ import {Recipe} from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
 
   @Input() recipe: Recipe;
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
+  toShoppingList() {
+    this.shoppingListService.addIngredients(this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,18 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>()
 
   private recipes: Recipe[] = [
-    new Recipe('recipe1Name', 'recipe1Desc', 'https://www.google.de/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'),
-    new Recipe('recipe2Name', 'recipe2Desc', 'https://www.google.de/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+    new Recipe('recipe1Name', 'recipe1Desc',
+      'https://www.google.de/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+      [new Ingredient('ing1',4), new Ingredient('ing3', 8)]),
+    new Recipe('recipe2Name', 'recipe2Desc',
+      'https://www.google.de/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+      [new Ingredient('ingx',4), new Ingredient('ingt', 8)])
   ];
 
   getRecipes() {
